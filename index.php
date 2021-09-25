2e4713a87f5a945ec094a2dbb782fa9fb510ccac
rashf0rd <nguyenminhtrong12a0@gmail.com>
2021-09-25 16:06:42 +0700

    tao moi web

---
+++ index.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link href="css/style.css" rel="Stylesheet">
+    <title>Thực Hành bootstrap</title>
+</head>
+
+<body>
+    <div class="header">
+        <div class="container-fluid">
+            <div class="text-center">
+                <div class="col-md-12 logo-info">
+                    <img src="image/logo.png" alt="" class="rounded">
+                </div> <!-- logo -->
+                <div class="col-md-12 menu-info">
+                    <a href="index.php">Home</a>
+                    <a href="category-foods.php">Categories</a>
+                    <a href="order.php">Food</a>
+                    <a href="index.php">Contact</a>
+                </div> <!-- Menu -->
+            </div>
+        </div>
+    </div>
+    <div class="find-food text-center bottom">
+        <div class="container-fluid">
+            <form action="">
+                <input type="search" name="search" placeholder="Search for food...">
+                <input type="submit" name="submit" value="Search" class="btn btn-prim">
+            </form>
+        </div>
+    </div>
+    
+        <!-- Optional JavaScript; choose one of the two! -->
+
+        <!-- Option 1: Bootstrap Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+
+        <!-- Option 2: Separate Popper and Bootstrap JS -->
+        <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
