816bbededbe3c544d10b77e64f252639bc4e9755
rashf0rd <nguyenminhtrong12a0@gmail.com>
2021-09-25 16:35:31 +0700

    design home

--- index.php
+++ index.php
@@ -28,41 +28,144 @@
             </div>
         </div>
     </div>
-    <div class="find-food text-center bottom">
-        <div class="container-fluid">
+
+    <section class="food-search text-center">
+        <div class="container">
             <form action="">
                 <input type="search" name="search" placeholder="Search for food...">
                 <input type="submit" name="submit" value="Search" class="btn btn-prim">
             </form>
         </div>
-    </div>
+    </section>
+
     <section class="explore_food">
         <div class="container-fluid">
             <h2 class="text-center"> Explore Food</h>
 
                 <div>
                     <a href="category-foods.php">
-                        <div class="box-3 float-container">
+                        <div class="box-3 float-container-fluid">
                             <img src="image/pizza.jpg" alt="Pizza" class="img-responsive img-curve">
-                            <h3 class="float-text text-blue">Pizza</h3>
+                            <h3 class="float-text text-white">Pizza</h3>
                         </div>
                     </a>
 
                     <a href="category-foods.php">
-                        <div class="box-3 float-container">
+                        <div class="box-3 float-container-fluid">
                             <img src="image/burger.jpg" alt="Bugger" class="img-responsive img-curve">
-                            <h3 class="float-text text-blue">Bugger</h3>
+                            <h3 class="float-text text-white">Bugger</h3>
                         </div>
                     </a>
 
                     <a href="category-foods.php">
-                        <div class="box-3 float-container">
+                        <div class="box-3 float-container-fluid">
                             <img src="image/momo.jpg" alt="Momo" class="img-responsive img-curve">
-                            <h3 class="float-text text-blue">Dumpling</h3>
+                            <h3 class="float-text text-white">Dumpling</h3>
                         </div>
                         <div class="clear-float"></div>
                     </a>
                 </div>
+        </div>
+    </section>
+
+    <section class="food-menu">
+        <div class="container-fluid">
+            <h2 class="text-center">Food menu</h2>
+        </div>
+        <div class="food-menu-box">
+            <div class="food-menu-img">
+                <img src="image/menu-pizza.jpg" alt="Pizza" class="img-responsive img-curve">
+            </div>
+            <div class="food-menu-decrip">
+                <h4>Food title</h4>
+                <p>$2.5</p>
+                <p>Made with Italian Sauce, Chicken, and organice vegetable</p>
+                <br>
+                <a href="order.php" class="btn btn-prim">Order Now</a>
+            </div>
+        </div>
+        <div class="food-menu-box">
+            <div class="food-menu-img">
+                <img src="image/menu-burger.jpg" alt="Burger" class="img-responsive img-curve">
+            </div>
+            <div class="food-menu-decrip">
+                <h4>Nice Buger</h4>
+                <p>$2.5</p>
+                <p>Made with Italian Sauce, Chicken, and organice vegetable</p>
+                <br>
+                <a href="order.php" class="btn btn-prim">Order Now</a>
+            </div>
+        </div>
+        <div class="food-menu-box">
+            <div class="food-menu-img">
+                <img src="image/menu-pizza.jpg" alt="Pizza" class="img-responsive img-curve">
+            </div>
+            <div class="food-menu-decrip">
+                <h4>Food title</h4>
+                <p>$2.5</p>
+                <p>Made with Italian Sauce, Chicken, and organice vegetable</p>
+                <br>
+                <a href="order.php" class="btn btn-prim">Order Now</a>
+            </div>
+        </div>
+        <div class="food-menu-box">
+            <div class="food-menu-img">
+                <img src="image/menu-burger.jpg" alt="Burger" class="img-responsive img-curve">
+            </div>
+            <div class="food-menu-decrip">
+                <h4>Food title</h4>
+                <p>$2.5</p>
+                <p>Made with Italian Sauce, Chicken, and organice vegetable</p>
+                <br>
+                <a href="order.php" class="btn btn-prim">Order Now</a>
+            </div>
+        </div>
+        <div class="food-menu-box">
+            <div class="food-menu-img">
+                <img src="image/menu-momo.jpg" alt="Momo" class="img-responsive img-curve">
+            </div>
+            <div class="food-menu-decrip">
+                <h4>Food title</h4>
+                <p>$2.5</p>
+                <p>Made with Italian Sauce, Chicken, and organice vegetable</p>
+                <br>
+                <a href="order.php" class="btn btn-prim">Order Now</a>
+            </div>
+        </div>
+        <div class="food-menu-box">
+            <div class="food-menu-img">
+                <img src="image/menu-momo.jpg" alt="Momo" class="img-responsive img-curve">
+            </div>
+            <div class="food-menu-decrip">
+                <h4>Food title</h4>
+                <p>$2.5</p>
+                <p>Made with Italian Sauce, Chicken, and organice vegetable</p>
+                <br>
+                <a href="order.php" class="btn btn-prim">Order Now</a>
+            </div>
+        </div>
+        <div class="clear-float"></div>
+    </section>
+
+    <section class="social">
+        <div class="container-fluid text-center ">
+            <ul>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/color/48/000000/facebook.png" /></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/color/48/000000/instagram-new--v1.png" /></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/color/48/000000/twitter--v2.png" /></a>
+                </li>
+            </ul>
+        </div>
+    </section>
+
+    <section class="footer">
+        <div class="container-fluid text-center">
+            <p>All rights reserved.</p>
         </div>
     </section>
 
